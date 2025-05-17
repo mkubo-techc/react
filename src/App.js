dcdf10866b43d383fa1c4956d47ddc4f81e927dd
DESKTOP-TN66GDV\mymai <mm.katatumuri.48@gmail.com>
2025-05-17 10:59:11 +0900

    Messageの変更

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {/* こんにちは、React！！ */}
+          {/* Helloは、React！！ */}
           Learn React
         </a>
       </header>
